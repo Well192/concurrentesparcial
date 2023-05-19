c766f2d7bbd34d03d56232c6b413c85544c7a587
Welll192 <maxwell.uni19.2@gmail.com>
2023-05-18 22:34:12 -0500

    parcial de concurrentes

---
+++ src/main/java/Cliente50.java
@@ -0,0 +1,132 @@
+import java.util.Scanner;
+
+class Cliente50 {
+
+    public double[] sum = new double[40];
+    TCPClient50 mTcpClient;
+    Scanner sc;
+
+    public static void main(String[] args) {
+        Cliente50 objcli = new Cliente50();
+        objcli.iniciar();
+    }
+    void iniciar() {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mTcpClient = new TCPClient50("127.0.0.1",
+                                new TCPClient50.OnMessageReceived() {
+                                    @Override
+                                    public void messageReceived(String message) {
+                                        ClienteRecibe(message);
+                                    }
+                                }
+                        );
+                        mTcpClient.run();
+                    }
+                }
+        ).start();
+        //---------------------------
+
+        String salir = "n";
+        sc = new Scanner(System.in);
+        System.out.println("Cliente bandera 01");
+        while (!salir.equals("s")) {
+            salir = sc.nextLine();
+            ClienteEnvia(salir);
+        }
+        System.out.println("Cliente bandera 02");
+
+    }
+
+    void ClienteRecibe(String llego) {
+        System.out.println("CLINTE50 El mensaje::" + llego);
+        if (llego.trim().contains("evalua")) {
+            String[] arrayString = llego.split("\\s+");
+            String polinomio = arrayString[1];
+            int a = Integer.parseInt(arrayString[2]);
+            int b = Integer.parseInt(arrayString[3]);
+            int cantidad =  Integer.parseInt(arrayString[4]);
+            int rango = Integer.parseInt(arrayString[5]);
+            System.out.println("El polinomio: "+polinomio+", el min:" + a + " el max:" + b+", "+cantidad);
+            procesar(polinomio,a, b,cantidad, rango);
+        }
+    }
+
+    void ClienteEnvia(String envia) {
+        if (mTcpClient != null) {
+            mTcpClient.sendMessage(envia);
+        }
+    }
+
+    double funcion(int fin) {
+        double sum = 0;
+        for (int j = 0; j <= fin; j++) {
+            sum = sum + Math.sin(j * Math.random());
+        }
+        return sum;
+    }
+
+    void procesar(String polinomio, double a, double b, int cantidad, int rango) {
+
+        int numIntervalos = (int) (((b-a)*cantidad)/rango);
+
+        int H = 6;//luego aumentar
+
+        double dx = (double) (rango) / cantidad;
+
+        int limite = numIntervalos/H;
+
+        Thread[] todos = new Thread[40];
+        for (int i = 0; i < (H - 1); i++) {
+
+            todos[i] = new tarea0101((i * dx*limite + a), ((i * dx*limite) +( dx*limite) + a), i,dx, polinomio);
+            todos[i].start();
+        }
+
+        todos[H - 1] = new tarea0101(((dx*limite * (H - 1)) + a), (b), H - 1,dx, polinomio);
+        todos[H - 1].start();
+        for (int i = 0; i <= (H - 1); i++) {//AQUI AQUI VER <=
+            try {
+                todos[i].join();
+            } catch (InterruptedException ex) {
+                System.out.println("error" + ex);
+            }
+        }
+        double sumatotal = 0;
+        for (int i = 0; i < H; i++) {
+            System.out.println(sum[i]);
+            sumatotal = sumatotal + sum[i];
+        }
+
+        ClienteEnvia("rpta " + sumatotal*dx);
+    }
+    public class tarea0101 extends Thread {
+        public double max, min;
+        int id;
+        double dx;
+
+        String polinomio;
+        tarea0101(double min_, double max_, int id_, double dx,String polinomio_) {
+            max = max_;
+            min = min_;
+            id = id_;
+            this.dx = dx;
+            polinomio = polinomio_;
+        }
+        public void run() {
+            double suma = 0;
+
+            for (double i = min; i < max; i=i+dx) {
+                Polinomio polinomio = new Polinomio(this.polinomio,i);
+                suma += polinomio.imprimirCoeficientes();
+                System.out.println("SUMAAAAAAA: "+ suma);
+            }
+            sum[id] = suma;
+            System.out.println(" min:" + min + " max:" + (max ) + " id:" + id + " suma:" + (suma));
+            // envio 7x^1+8x^2 5 10 1000
+        }
+    }
+
+}
---
+++ src/main/java/Polinomio.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Polinomio {
+    private final double[] coeficientes;
+    private final double x;
+    public Polinomio(String polinomio, double numero) {
+
+
+        x = numero;
+
+        // Dividir el polinomio en términos individuales
+        String[] terminos = polinomio.split("\\s*\\+\\s*|\\s*\\-\\s*");
+
+        // Determinar el grado máximo del polinomio
+        int gradoMaximo = 0;
+        for (String termino : terminos) {
+            int grado = obtenerGrado(termino);
+            gradoMaximo = Math.max(grado, gradoMaximo);
+        }
+
+        // Inicializar el arreglo de coeficientes
+        coeficientes = new double[gradoMaximo + 1];
+
+        // Asignar los coeficientes en el arreglo
+        for (String termino : terminos) {
+            double coeficiente = obtenerCoeficiente(termino);
+            int grado = obtenerGrado(termino);
+            coeficientes[grado] = coeficiente;
+        }
+    }
+
+    public double imprimirCoeficientes() {
+        double resultado = 0.;
+
+        for (int i = 0; i < coeficientes.length; i++) {
+
+            resultado += coeficientes[i] * Math.pow(x, i);
+        }
+
+        System.out.println(resultado);
+        return resultado;
+    }
+
+    private double obtenerCoeficiente(String termino) {
+        String coeficienteStr = termino.split("x")[0];
+        if (coeficienteStr.isEmpty()) {
+            return 1.0;
+        } else if (coeficienteStr.equals("-")) {
+            return -1.0;
+        } else {
+            return Double.parseDouble(coeficienteStr);
+        }
+    }
+
+    private int obtenerGrado(String termino) {
+        if (termino.contains("^")) {
+            String gradoStr = termino.split("\\^")[1];
+            return Integer.parseInt(gradoStr);
+        } else if (termino.contains("x")) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        double x=5;
+        double dx= (double) (10-5)/200;
+        double respuesta = 0;
+        for(double i=5+6*dx; i < 5+12*dx;  i = i+dx ){ // 200 intervalos
+            Polinomio polinomio = new Polinomio("7x^1+8x^2",i);
+            respuesta += polinomio.imprimirCoeficientes();
+
+        }
+
+        System.out.println("La respuesta es: "+(respuesta ));
+
+    }
+}
